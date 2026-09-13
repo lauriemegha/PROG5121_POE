58ae1d7b083bfc36edc8dcd26a889a31e3582158
lauriemegha <meghalaurie@gmail.com>
2026-09-13 22:11:38 +0200

    Updated the Main and Login file to be optimised for the future tests.

---
+++ src/main/java/com/example/Login.java
@@ -0,0 +1,82 @@
+package com.example;
+
+public class Login {
+    private String username;
+    private String password;
+    private String phoneNumber;
+    private String firstName;
+    private String lastName;
+
+    // Constructor to set up the registration fields
+    public Login(String username, String password, String phoneNumber, 
+                 String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Checks that the username contains an underscore and is 5 characters or less
+    public boolean checkUserName() {
+        return username != null && 
+               username.contains("_") && 
+               username.length() <= 5;
+    }
+
+    // Validates password complexity against required rules
+    public boolean checkPasswordComplexity() {
+        if (password == null || password.length() < 8) return false;
+        boolean upper = false, digit = false, special = false;
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) upper = true;
+            else if (Character.isDigit(c)) digit = true;
+            else if (!Character.isLetterOrDigit(c)) special = true;
+        }
+        return upper && digit && special;
+    }
+
+    // Validates international phone formatting (+27 prefix)
+    public boolean checkPhoneNumber() {
+        return phoneNumber != null && 
+               phoneNumber.matches("^\\+27\\d{9,10}$");
+    }
+
+    // Generates registration messages per assignment string tables
+    public String registerUser() {
+        String uMsg = checkUserName() ? 
+            "Username successfully captured." : 
+            "Username is not correctly formatted, please ensure that "
+            + "your username contains an underscore and is no more "
+            + "than 5 characters in length .";
+        
+        String pMsg = checkPasswordComplexity() ? 
+            "Password successfully captured" : 
+            "Password is not correctly formatted, please ensure that "
+            + "the password contains at least 8 characters, "
+            + "a capital letter, a number and a special character.";
+            
+        String phMsg = checkPhoneNumber() ? 
+            "Cell phone number successfully added." : 
+            "Cell phone number incorrectly formatted or does not contain "
+            + "an international code.";
+
+        return uMsg + "\n" + pMsg + "\n" + phMsg;
+    }
+
+    // Authenticates user match credentials
+    public boolean loginUser(String inputUsername, String inputPassword) {
+        return this.username.equals(inputUsername) && 
+               this.password.equals(inputPassword);
+    }
+
+    // Returns structural user greeting message arrays
+    public String returnLoginStatus(boolean loginSuccessful) {
+        if (loginSuccessful) {
+            return "Welcome " + firstName + " ," + lastName 
+                 + " it is great to see you again.";
+        } else {
+            return "Username or password incorrect, please try again";
+        }
+    }
+}
---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("--- REGISTRATION ---");
+        System.out.println("Enter First Name: "); String fName = scanner.nextLine();
+        System.out.println("Enter Last Name: ");  String lName = scanner.nextLine();
+        System.out.println("Enter Username: ");   String uName = scanner.nextLine();
+        System.out.println("Enter Password: ");   String pWord = scanner.nextLine();
+        System.out.println("Enter Phone Number: "); String phone = scanner.nextLine();
+
+        // Initialize login instance properties
+        Login user = new Login(uName, pWord, phone, fName, lName);
+        System.out.println("\n" + user.registerUser());
+        
+        // Cascades forward to the entry flow if validation flags are valid
+        if (user.checkUserName() && 
+            user.checkPasswordComplexity() && 
+            user.checkPhoneNumber()) {
+            
+            System.out.println("\n--- LOGIN USER ---");
+            System.out.println("Username: "); String inUser = scanner.nextLine();
+            System.out.println("Password: "); String inPass = scanner.nextLine();
+
+            boolean isAuthenticated = user.loginUser(inUser, inPass);
+            System.out.println("\n" + user.returnLoginStatus(isAuthenticated));
+        } else {
+            System.out.println("\nRegistration metrics failed. App terminated.");
+        }
+        scanner.close();
+    }
+}
